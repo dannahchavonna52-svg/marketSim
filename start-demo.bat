ba07b3b01a3637e066a4b2e71d5a49d386ef23c2
dannahchavonna52-svg <dannahchavonna52@gmail.com>
2026-07-10 09:09:03 +0800

    Add one-click video demo launcher

---
+++ start-demo.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal
+cd /d "%~dp0backend"
+
+if not exist "data" mkdir "data"
+if not exist ".tmp" mkdir ".tmp"
+
+set "TEMP=%CD%\.tmp"
+set "TMP=%CD%\.tmp"
+set "PYTHONDONTWRITEBYTECODE=1"
+
+set "PYTHON_EXE=python"
+if exist ".venv\Scripts\python.exe" set "PYTHON_EXE=.venv\Scripts\python.exe"
+
+set "DEMO_URL=http://127.0.0.1:8000/?demo=1^&fund=014855^&focus=1"
+
+echo.
+echo MarketSim video demo is starting...
+echo Demo URL: %DEMO_URL%
+echo.
+
+start "MarketSim Demo" cmd /c "timeout /t 3 /nobreak >nul & start "" "%DEMO_URL%""
+"%PYTHON_EXE%" -m uvicorn main_ai:app --host 0.0.0.0 --port 8000
+
+pause
