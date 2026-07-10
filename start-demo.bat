0ed611e47189de9481ecbfa716835a5807991ee2
dannahchavonna52-svg <dannahchavonna52@gmail.com>
2026-07-10 09:10:09 +0800

    Harden one-click demo launcher quoting

--- start-demo.bat
+++ start-demo.bat
@@ -12,14 +12,14 @@ set "PYTHONDONTWRITEBYTECODE=1"
 set "PYTHON_EXE=python"
 if exist ".venv\Scripts\python.exe" set "PYTHON_EXE=.venv\Scripts\python.exe"
 
-set "DEMO_URL=http://127.0.0.1:8000/?demo=1^&fund=014855^&focus=1"
+set "DEMO_URL=http://127.0.0.1:8000/?demo=1&fund=014855&focus=1"
 
 echo.
 echo MarketSim video demo is starting...
 echo Demo URL: %DEMO_URL%
 echo.
 
-start "MarketSim Demo" cmd /c "timeout /t 3 /nobreak >nul & start "" "%DEMO_URL%""
+start "" powershell -NoProfile -WindowStyle Hidden -Command "Start-Sleep -Seconds 3; Start-Process $env:DEMO_URL"
 "%PYTHON_EXE%" -m uvicorn main_ai:app --host 0.0.0.0 --port 8000
 
 pause
